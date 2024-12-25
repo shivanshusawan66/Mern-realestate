c034960aeba08ef2073eb3793b78f60bce068c52
shivanshusawan66 <shivanshusawan66@gmail.com>
2024-12-25 20:34:49 +0530

    added header component & routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 const App = () => {
   return (
-    <div className="text-blue-500">
-      I am SHIVANSHU.....Sawandf
-    </div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    
+    </Routes>
+    </BrowserRouter>
   )
 }
 
