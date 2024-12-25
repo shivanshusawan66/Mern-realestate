b668601319d4e4fd9c8428ba4b9a52cf5a6eb4b4
shivanshusawan66 <shivanshusawan66@gmail.com>
2024-12-25 21:59:34 +0530

    Remove .env and node_modules from tracking

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+// HELLO 
 mongoose.connect(process.env.MONGO).then(() =>{
     console.log("connected to Mongodb");
     
